97064c54e9b4a0904b7179076276e972d22fe38b
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2025-12-02 21:08:49 +0100

    add styles for header
    - add styles for header
    - created container component

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,18 +1,34 @@
 import Link from "next/link";
+import css from "./Header.module.css";
+import Container from "../Container/Container";
+
 const Header = () => {
   return (
-    <header>
-      <a href="">TravelTracks</a>
-      <nav aria-label="Web navigation">
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/catalog">Catalog</Link>
-          </li>
-        </ul>
-      </nav>
+    <header className={css.header}>
+      <Container>
+        <div className={css.headerContainer}>
+          <Link href="/">
+            <svg width="138" height="16" className={css.logo}>
+              <use href="/symbol-defs.svg#icon-main_logo"></use>
+            </svg>
+          </Link>
+
+          <nav className={css.headerNav} aria-label="Web navigation">
+            <ul className={css.headerNavList}>
+              <li className={css.headerNavItem}>
+                <Link className={css.headerNavLink} href="/">
+                  Home
+                </Link>
+              </li>
+              <li className={css.headerNavItem}>
+                <Link className={css.headerNavLink} href="/catalog">
+                  Catalog
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </Container>
     </header>
   );
 };
